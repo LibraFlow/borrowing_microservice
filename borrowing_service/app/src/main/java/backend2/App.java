d022be75ea759795e0e9366a5cf5e0cc36fa35b3
i459452 <459452@student.fontys.nl>
2025-04-30 14:46:46 +0300

    Revision Test

--- borrowing_service/app/src/main/java/backend2/App.java
+++ borrowing_service/app/src/main/java/backend2/App.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class App {
     @GetMapping("/")
     public String getGreeting() {
-        return "Hello World! Borrowing Service Revision 2!";
+        return "Hello World! Borrowing Service Revision 3!";
     }
 
     public static void main(String[] args) {
